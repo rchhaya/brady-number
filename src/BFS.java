5448647b44c5caf844b96346390a565332fb63db
rohan <rchhaya@seas.upenn.edu>
2021-05-01 16:22:20 -0400

    Updates minus parser

---
+++ src/BFS.java
@@ -0,0 +1,76 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class BFS {
+    
+    private HashMap<Player,ArrayList<Player>> graph = new HashMap<Player,ArrayList<Player>>();
+    private Player src, tgt;
+    private HashMap<Player,Player> parent;
+
+    public BFS(HashMap<Player,ArrayList<Player>> graph, Player src, Player tgt) {
+        this.graph = graph;
+        this.src = src;
+        this.tgt = tgt;
+        parent = new HashMap<Player,Player>();
+    }
+    
+    public ArrayList<Player> bfsTraversal(Player src, Player tgt) {
+        
+        ArrayList<Player> path = new ArrayList<Player>();
+        
+        //Initialize the player HashMap for each player 
+        for (Map.Entry<Player, ArrayList<Player>> entry : graph.entrySet()) {
+            parent.put(entry.getKey(), null);
+        }
+        
+        ArrayList<Player> discovered = new ArrayList<Player>();
+        ArrayList<Player> queue = new ArrayList<Player>();
+        ArrayList<Player> layer = new ArrayList<Player>();
+        ArrayList<Player> nextLayer = new ArrayList<Player>();
+        
+        //Account for the src/first player
+        queue.add(src);
+        layer.add(src);
+            
+        while (!queue.isEmpty()) {
+            
+            //For each layer 
+            for (int i = 0; i < layer.size(); i++) {
+                
+                Player curr = queue.get(0);
+                
+                queue.remove(0);
+                
+                for (Player p : graph.get(curr)) {
+                    if (!discovered.contains(p)) {
+                        discovered.add(p);
+                        queue.add(p);
+                        parent.put(curr, p);
+                        nextLayer.add(p);
+                    }
+                }
+            }
+            
+            layer = nextLayer;
+            nextLayer = new ArrayList<Player>();
+            
+        }
+        
+        //Create the path by traversing the parents from tgt to src Player
+        
+        Player curr = tgt;
+        path.add(0,curr);
+        while (curr != src) {
+            curr = parent.get(curr);
+            if (curr != src && curr == null) {
+                return new ArrayList<Player>();
+            }
+            path.add(0,curr);
+        }
+        
+        return path;
+        
+    }
+    
+}
--- src/BaconNumber.java
+++ src/BaconNumber.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -19,19 +20,28 @@ public class BaconNumber extends JFrame implements Runnable {
     
     private JLabel statusBar = new JLabel();
     static String username = "";
-    private JTextField userInput = new JTextField("",20);
+    private JTextField userInput1 = new JTextField("",20);
+    private JTextField userInput2 = new JTextField("", 20);
     private boolean running;
     
     public void run() {
+    	//PlayerParser parser = new PlayerParser();
+    	//ArrayList<Player> playerList = parser.playerList();
+    	//Parser instance 
+    	//access the master list 
+    	
+    	AdjacencyList aList = new AdjacencyList(new ArrayList<Player>());
         
         running = true;
         
-        statusBar = new JLabel("Bacon Number: "); 
+        statusBar = new JLabel("Brady Number: "); 
         
         //Frame Label
         final JFrame frame = new JFrame("Bacon Number");
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         frame.setLocation(screenSize.width / 3, screenSize.height / 3);
+        
+        
         
         //Status Panel
         JPanel statusPanel = new JPanel();
@@ -40,13 +50,21 @@ public class BaconNumber extends JFrame implements Runnable {
         statusPanel.add(statusBar);
         
 
-        JPanel controlPanel = new JPanel();
-        frame.add(controlPanel, BorderLayout.NORTH);
+        JPanel controlPanel1 = new JPanel();
+        frame.add(controlPanel1, BorderLayout.NORTH);
+        
+        JPanel controlPanel2 = new JPanel();
+        frame.add(controlPanel2, BorderLayout.CENTER);
+        
+        JPanel controlPanel3 = new JPanel();
+        frame.add(controlPanel3, BorderLayout.SOUTH);
         
         //Reset Button
         final JButton reset = new JButton("Reset");
         reset.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
+            	userInput1.setText("");
+            	userInput2.setText("");
                 
             }
         });
@@ -59,11 +77,15 @@ public class BaconNumber extends JFrame implements Runnable {
         });
         
         //Menu Button
-        final JButton menu = new JButton("Menu");
+        final JButton menu = new JButton("Directions");
         menu.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e3) {
                 JOptionPane.showMessageDialog(null,
-                        "Instructions Here",
+                        "Enter 2 current NFL players in the spaces provided.\n"
+                        + "Please enter their full first and last name (no middle"
+                        + " name or suffixes) with title-case capitalization \n"
+                        + " as well as their 2-letter position abbreviation \n(ex. Tom"
+                        + " Brady QB)",
                         "Instructions", JOptionPane.INFORMATION_MESSAGE);
             }
         });
@@ -75,21 +97,65 @@ public class BaconNumber extends JFrame implements Runnable {
         
         enter.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e4) {
-    
+            	//Check to make sure strings are long enough
+            	String entry1 = userInput1.getText();
+            	String name1 = entry1.substring(0, entry1.length()-2);
+            	String position1 = entry1.substring(entry1.length()-2, entry1.length());
+            	System.out.println(name1);
+            	System.out.println(position1);
+            	String entry2 = userInput2.getText();
+            	String name2 = entry2.substring(0, entry2.length()-2);
+            	String position2 = entry2.substring(entry2.length()-2, entry2.length());
+            	System.out.println(name2);
+            	System.out.println(position2);
+            	/*Player player1 = new Player();
+            	 * Player player2 = new Player();
+            	 * boolean assigned1 = false;
+            	 * boolean assigned2 = false;
+            	 * for (Player p : playerList){
+            	 *   if (p.name.equals(name1) && p.position.equals(position1) && !assigned1){
+            	 *   player1 = p;
+            	 *   assigned1 = true;
+            	 *   continue;
+            	 *   }     
+            	 *   if (p.name.equals(name2) && p.position.equals(position2) && !assigned2){
+            	 *   player2 = p;
+            	 *   assigned2 = true
+            	 *   }       	 
+            	 * }
+            	 * 
+            	 * BFS obj = new BFS(player1, player2 aList)
+            	 * 
+            	 * obj.getPath(); to give the path of players 
+            	 * 
+            	 *make a dialog box to show the path 
+            	 * 
+            	 */
+            	
+            	JOptionPane.showMessageDialog(null,
+                        "Enter 2 current NFL players in the spaces provided.\n"
+                        + "Please enter their full first and last name (no middle"
+                        + " name or suffixes) with title-case capitalization \n"
+                        + " as well as their 2-letter position abbreviation \n(ex. Tom"
+                        + " Brady QB)",
+                        "Instructions", JOptionPane.INFORMATION_MESSAGE);
             }
         });
         
         //Add buttons to the panels  
-        controlPanel.add(userInput);
-        controlPanel.add(enter); 
-        controlPanel.add(reset);
-        controlPanel.add(menu);
+        controlPanel1.add(userInput1);
+
+        controlPanel2.add(userInput2);
+        
+        controlPanel3.add(enter); 
+        controlPanel3.add(reset);
+        controlPanel3.add(menu);
         
         //Make visible on frame
         frame.pack();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
-        frame.setSize(500,100);
+       // frame.setSize(500,100);
         
         
     }
@@ -100,7 +166,7 @@ public class BaconNumber extends JFrame implements Runnable {
     
     public static void main(String[] args) {
         BaconNumber b = new BaconNumber();
-        b.setLocationRelativeTo(null);
+        //b.setLocationRelativeTo(null);
         b.setVisible(true);
         SwingUtilities.invokeLater(new BaconNumber());
     }
